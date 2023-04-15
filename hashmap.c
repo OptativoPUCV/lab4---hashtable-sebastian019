40d6b7de1744fb44bbf472931c7ce4322fc0e656
Replit user <>
2023-04-15 19:41:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -127,7 +127,7 @@ Pair * nextMap(HashMap * map) {
       map -> current = pos;
       return map -> buckets[pos];
     }
-    pos++;
+    pos = ((pos + 1) % map -> capacity);
   }
   return NULL;
 }
